5fa21d7bc33f89aae2649692ec2c5f0ac23ca729
KateO-R <veg2710@gmail.com>
2024-10-02 17:23:01 +0500

    new lesson code

--- part1.py
+++ part1.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv('animal.csv')
+data = {
+    'Age': [23, 30, 22, 27, 21, 20, 29, 28, 22, 25],
+    'Salary': [54000, 58000, 60000, 52000, 55000, 59000, 51000, 49000, 53000, 61000],
+}
 
-print(df)
+df = pd.DataFrame(data)
 
-group = df.groupby('Пища')['Средняя продолжительность жизни'].mean()
-print(group)
+print(f'Average age - {df['Age'].mean()}')
+print(f'Median age - {df['Age'].median()}')
+print(f'Standard deviation of the age - {df['Age'].std()}')
+
+print(f'Average salary - {df['Salary'].mean()}')
+print(f'Median salary - {df['Salary'].median()}')
+print(f'Standard deviation of the salary - {df['Salary'].std()}')
